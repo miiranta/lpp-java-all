1e8bc5fb53db5ed078cc48de48f38e75fd1a7789
Lucas Miranda <lucasrezende12@hotmail.com>
2021-09-09 22:13:38 -0300

    9

---
+++ 10 - Final Test/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        //Impossible to change value, its like a constant
+        //Final.b = "lol";
+
+        System.out.println(Final.b);
+
+    }
+}
