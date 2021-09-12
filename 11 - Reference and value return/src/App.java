fbe5895e6e0d753c95abb1fcf5ee812d825e3bb6
Lucas Miranda <lucasrezende12@hotmail.com>
2021-09-12 18:57:15 -0300

    11

---
+++ 11 - Reference and value return/src/App.java
@@ -0,0 +1,34 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        //Setting default
+        int array[] = new int[10];
+        int a = 1;
+        array[0] = 1;
+
+        //Print before
+        System.out.println("a = " + a);
+        System.out.println("array = " + array[0]);
+        System.out.println();
+
+        //Change
+        changeValue(a);
+        changeReference(array);
+
+        //Print after
+        System.out.println("a = " + a);
+        System.out.println("array = " + array[0]);
+        System.out.println();
+
+    }
+
+    public static void changeValue(int a){
+        a++;
+    }
+
+    public static void changeReference(int array[]){
+        //array = new int[2];  //If on, reference stops working
+        array[0]++;
+        //array = new int[2]; //Doesnt make any difference
+    }
+}
